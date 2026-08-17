2269fdafe3037a1c0d55be029e3245fd8ea6d952
Jennifer Sheng <122578357+sheng52@users.noreply.github.com>
2026-08-17 12:18:46 -0700

    10. Why React? It is Declarative!

--- 01. Static pages/10. Why React? It is Declarative!/index.jsx
+++ 01. Static pages/10. Why React? It is Declarative!/index.jsx
@@ -19,3 +19,7 @@ Don't use innerHTML to accomplish any of this.
     
 */
 
+const h1 = document.createElement("h1");
+h1.textContent = 'Hello, React!';
+h1.className = 'header';
+document.getElementById('root').appendChild(h1);
