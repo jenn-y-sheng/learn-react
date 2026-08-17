41b9d8513909c42f15ea93bdf4f591d4479e4ebb
Jennifer Sheng <122578357+sheng52@users.noreply.github.com>
2026-08-16 22:05:53 -0700

    04. First React Challenge

--- 01. Static pages/04. First React Challenge/index.jsx
+++ 01. Static pages/04. First React Challenge/index.jsx
@@ -3,3 +3,13 @@
  * This time, try rendering an unordered list with 2-3 list items inside
  * with why you're excited to be learning React.
  */
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <ul>
+    <li>I need a job</li>
+    <li>I need a job</li>
+    <li>I need a job</li>
+  </ul>
+);
