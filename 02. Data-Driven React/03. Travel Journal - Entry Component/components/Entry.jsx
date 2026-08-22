5e89b511b263a5890905a9eb5b43268b5aea8c88
Jennifer Sheng <122578357+sheng52@users.noreply.github.com>
2026-08-22 12:58:05 -0700

    3-10

--- 02. Data-Driven React/03. Travel Journal - Entry Component/components/Entry.jsx
+++ 02. Data-Driven React/03. Travel Journal - Entry Component/components/Entry.jsx
@@ -13,3 +13,18 @@
  *   currently are, so don't worry about the fact that you're hard-coding all
  *   this data into the component.
  */
+export default function Entry() {
+  return (
+    <article className="journal-entry">
+      <img className="main-image" src="https://scrimba.com/links/travel-journal-japan-image-url" alt="japan image" />
+      <div className="info">
+        <img className="marker-icon" src="../images/marker.png" alt="marker icon" />
+        <span>JAPAN</span>
+        <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">View on Google Maps</a>
+        <h1>Mount Fuji</h1>
+        <p className="date">12 Jan, 2021 - 24 Jan, 2021</p>
+        <p className="description">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+      </div>
+    </article>
+  );
+}
