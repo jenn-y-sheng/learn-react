52c04d54537043d805826410aa52c8733bed90fe
Jennifer Sheng <122578357+sheng52@users.noreply.github.com>
2026-08-24 12:53:50 -0700

    5-6

--- 03. React State/05. Event Listeners/index.jsx
+++ 03. React State/05. Event Listeners/index.jsx
@@ -5,6 +5,10 @@ function App() {
   function handleClick() {
     console.log("I was clicked!")
   }
+
+  function handleHover() {
+    console.log("I was hovered over!")
+  }
   
   /**
    * Challenge: add our new function to the button
@@ -15,8 +19,9 @@ function App() {
       <img
         src="https://picsum.photos/640/360"
         alt="Placeholder image from Picsum"
+        onMouseEnter={handleHover}
       />
-      <button onClick={}>Click me</button>
+      <button onClick={handleClick}>Click me</button>
     </main>
   )
 }
--- 03. React State/06. Chef Claude - Map ingredients list/Main.jsx
+++ 03. React State/06. Chef Claude - Map ingredients list/Main.jsx
@@ -8,19 +8,34 @@ export default function Main() {
      * Note: We're doing things a weird way here. Don't worry,
      * we're building up to learning the right way 🙂
      */
+
+    const listItems = ingredients.map(i => (
+        <li key={i}>{i}</li>
+    ))
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        // console.log("Form submitted!")
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        // console.log(newIngredient)
+        ingredients.push(newIngredient)
+        console.log(ingredients)
+    }
     
     return (
         <main>
-            <form className="add-ingredient-form">
+            <form className="add-ingredient-form" onSubmit={handleSubmit}>
                 <input 
                     type="text"
                     placeholder="e.g. oregano"
                     aria-label="Add ingredient"
+                    name="ingredient"
                 />
                 <button>Add ingredient</button>
             </form>
             <ul>
-                {/* Render ingredientsList here */}
+                {listItems}
             </ul>
         </main>
     )
